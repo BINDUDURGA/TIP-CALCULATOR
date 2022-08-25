ac801b34aff337cf7314e62a0d289cbc500ddbd7
BINDUDURGA <bindusaidurga@gmail.com>
2022-08-25 18:59:04 +0530

    100%done

--- script.js
+++ script.js
@@ -14,5 +14,19 @@ function calculateTip(){
     
     billInput.value = bill.toFixed(2);
 
-    let totalTip = parseFloat((BILL * (tipPercent/100)).toFixed(2));
+    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+
+    let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
+    let totalPerPerson = (total / noOfPeople).toFixed(2);
+
+    document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
+    document.getElementById("tip-amount").textContent = `\$ ${total}`;
+
+    document.getElementById("tip-percent").textContent = `\$ ${tipPercent}%`;
+    document.getElementById("split-num").textContent = noOfPeople;
+
+    document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}%`; 
+    document.getElementById("total-per-person").textContent = `\$ ${totalPerPerson}%`; 
 }
+calculateTip();
